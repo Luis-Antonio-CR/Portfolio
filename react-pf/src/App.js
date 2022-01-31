e10e9bd418ad8b1e3a671920afd5a73c1bb4925f
Luis-Antonio-CR <96863185+Luis-Antonio-CR@users.noreply.github.com>
2022-01-31 19:05:59 +0100

    Implementetion of NavBar.js and the NavBar component

--- react-pf/src/App.js
+++ react-pf/src/App.js
@@ -1,18 +1,11 @@
-
+import React from 'react';
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <img src="images/cube.jpg" alt="" />
-
-      <ul>
-        <li key='list-01'>Home</li>
-        <li key='list-A'>/</li>
-        <li key='list-02'>About Me</li>
-        <li key='list-B'>/</li>
-        <li key='list-03'>Projects</li>
-      </ul>
+      <NavBar />
     </div>
   );
 }
---
+++ react-pf/src/components/NavBar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+import '../styles/NavBar.css';
+import Logo from "../assets/Logo_LSC.png";
+
+function NavBar() {
+  return (
+    <div className="NavBar">
+      <img src={Logo} alt="" />
+
+      <div className="Menu">
+        <Link to="/" className="links"> Home</Link>
+        <Link to="/aboutMe" className="links">About Me</Link>
+        <Link to="/project" className="links">Projects</Link>
+      </div>
+
+    </div>
+  )
+}
+
+export default NavBar;
