d0de13ed17df6b6fcceaf40af4ed87fe103d8b3b
Luis Antonio Castaneda <luisantonio9893@gmail.com>
2024-09-17 19:54:26 +0200

    Update abuot page

--- src/pages/home.js
+++ src/pages/home.js
@@ -6,32 +6,31 @@ import '../styles/home.css';
 import cubeImg from '../assets/cube.jpg'
 import largeImg from '../assets/large.jpg'
 import regularImg from '../assets/regular.jpg'
-import imgAboutMe from '../assets/About_Me.jpeg'
+import imgAboutMe from '../assets/sticker.webp'
 
 function Home() {
   return (
     <div className="main">
       <div className="box_hero" style={{/*backgroundImage: `url(${largeImg})`*/}}>
-        <h1 className="big_text">I'm a Front-End Web Developer</h1>
-        <h2 className="normal_text">Subtitle</h2>
+        <h1 className="big_text">Front-End <br></br> Web Developer</h1>
       </div>
-
-      <div className="box_aboutMe_home_img">
-        <img src={imgAboutMe} alt="" />
-      </div>
-      <div className="box_aboutMe_home_text">
-        <h2 className="medium_text">ABOUT ME</h2>
-        <p className="normal_text">
-          I'm a 26 years old guy from Italy. 
-        </p>
-        <Link to="/aboutMe" className="links button normal_text">Know more</Link>
-      </div>
-
+      
       <div className="box_projects">
         <h2 className="medium_text">PROJECTS</h2>
         <ProjectComp />
 
         <Link to="/projects" className="links button normal_text">For more projects</Link>
+      </div>
+
+      <div className="box_aboutMe_home_text">
+        <h2 className="medium_text">ABOUT ME</h2>
+        <p className="normal_text">
+          Hi, I'm Luis Antonio and I'm a frontend web developer studing at NAD Verona. 
+        </p>
+        <Link to="/aboutMe" className="links button normal_text">Know me better</Link>
+      </div>
+      <div className="box_aboutMe_home_img">
+        <img src={imgAboutMe} alt="" />
       </div>
 
       <div id="like_button_container"></div>
